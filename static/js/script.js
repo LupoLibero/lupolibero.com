301270430731e0cc0617244805f8f67770ca8906
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-05-16 17:42:59 +0200

    Optimization. and correct some problems.

--- static/js/script.js
+++ static/js/script.js
@@ -2,85 +2,44 @@
 var $ = jQuery.noConflict();
 
 $(document).ready(function($) {
+  var $hiddenHeader = $('.hidden-header');
+  var $backToTop    = $('.back-to-top');
+  var $html         = $('html, body');
+  var $topBar       = $('.top-bar');
+  var $header       = $('header');
+  var $navbarBrand  = $('.navbar-brand');
+  var offset        = 200;
+  var duration      = 500;
+
   /*----------------------------------------------------*/
   /*	Hidder Header
   /*----------------------------------------------------*/
-  var headerEle = function(){
-    var $headerHeight = $('header').height();
-    $('.hidden-header').css({ 'height' : $headerHeight  + "px" });
-  };
-  $(window).load(function () {
-    headerEle();
-  });
-  $(window).resize(function () {
-    headerEle();
-  });
-
-  /*----------------------------------------------------*/
-  /*	Nav Menu & Search
-  /*----------------------------------------------------*/
-  $(".nav > li:has(ul)").addClass("drop");
-  $(".nav > li.drop > ul").addClass("dropdown");
-  $(".nav > li.drop > ul.dropdown ul").addClass("sup-dropdown");
-  $('.show-search').click(function() {
-    $('.search-form').fadeIn(300);
-    $('.search-form input').focus();
-  });
-  $('.search-form input').blur(function() {
-    $('.search-form').fadeOut(300);
-  });
+  var $headerHeight = $header.height();
+  $hiddenHeader.css({ 'height' : $headerHeight  + "px" });
 
   /*----------------------------------------------------*/
   /*	Back Top Link
   /*----------------------------------------------------*/
-  var offset   = 200;
-  var duration = 500;
   $(window).scroll(function() {
     if ($(this).scrollTop() > offset) {
-      $('.back-to-top').fadeIn(400);
+      $backToTop.fadeIn(400);
     } else {
-      $('.back-to-top').fadeOut(400);
+      $backToTop.fadeOut(400);
     }
   });
-  $('.back-to-top').click(function(event) {
+  $backToTop.on('click', function(event) {
     event.preventDefault();
-    $('html, body').animate({scrollTop: 0}, 600);
+    $html.animate({scrollTop: 0}, 600);
     return false;
-  })
-
-  /*----------------------------------------------------*/
-  /*	Css3 Transition
-  /*----------------------------------------------------*/
-
-  $('*').each(function(){
-    if($(this).attr('data-animation')) {
-      var $animationName = $(this).attr('data-animation'),
-        $animationDelay = "delay-"+$(this).attr('data-animation-delay');
-      $(this).appear(function() {
-        $(this).addClass('animated').addClass($animationName);
-        $(this).addClass('animated').addClass($animationDelay);
-      });
-    }
-  });
-
-
-  /*----------------------------------------------------*/
-  /*	Nivo Lightbox
-  /*----------------------------------------------------*/
-  $('.lightbox').nivoLightbox({
-    effect: 'fadeScale',
-    keyboardNav: true,
-    errorMessage: 'The requested content cannot be loaded. Please try again later.'
   });
 
   /*----------------------------------------------------*/
   /*	Sticky Header
   /*----------------------------------------------------*/
   (function() {
-    var docElem = document.documentElement,
-      didScroll = false,
-      changeHeaderOn = 100;
-      document.querySelector( 'header' );
+    var docElem        = document.documentElement;
+    var didScroll      = false;
+    var changeHeaderOn = 70;
 
     function init() {
       window.addEventListener( 'scroll', function() {
@@ -94,30 +53,20 @@ $(document).ready(function($) {
     function scrollPage() {
       var sy = scrollY();
       if ( sy >= changeHeaderOn ) {
-        $('.top-bar').slideUp(300);
-        $("header").addClass("fixed-header");
-        $('.navbar-brand').css({ 'padding-top' : 19 + "px", 'padding-bottom' : 19 + "px" });
-
-        if (/iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || $(window).width() < 479 ){
-          $('.navbar-default .navbar-nav > li > a').css({ 'padding-top' : 0 + "px", 'padding-bottom' : 0 + "px" })
-        }else{
-          $('.navbar-default .navbar-nav > li > a').css({ 'padding-top' : 20 + "px", 'padding-bottom' : 20 + "px" })
-          $('.search-side').css({ 'margin-top' : -7 + "px" });
-        };
-
+        $topBar.stop().slideUp(300);
+        $header.addClass('fixed-header');
+        $navbarBrand.css({
+          'padding-top':    19 + 'px',
+          'padding-bottom': 19 + 'px',
+        });
       }
       else {
-        $('.top-bar').slideDown(300);
-        $("header").removeClass("fixed-header");
-        $('.navbar-brand').css({ 'padding-top' : 27 + "px", 'padding-bottom' : 27 + "px" });
-
-        if (/iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || $(window).width() < 479 ){
-          $('.navbar-default .navbar-nav > li > a').css({ 'padding-top' : 0 + "px", 'padding-bottom' : 0 + "px" })
-        }else{
-          $('.navbar-default .navbar-nav > li > a').css({ 'padding-top' : 28 + "px", 'padding-bottom' : 28 + "px" })
-          $('.search-side').css({ 'margin-top' : 0  + "px" });
-        };
-
+        $topBar.stop().slideDown(300);
+        $header.removeClass('fixed-header');
+        $navbarBrand.css({
+          'padding-top':     27 + 'px',
+          'padding-bottom':  27 + 'px',
+        });
       }
       didScroll = false;
     }
